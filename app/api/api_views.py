71a7cfe8dd84f41481f23c7287b42186e087737b
Kuchuk Egor <kriptyashka@gmail.com>
2024-11-15 16:38:33 +0300

    Удалены неиспользуемые библиотеки

--- app/api/api_views.py
+++ app/api/api_views.py
@@ -1,6 +1,4 @@
 from django.core.handlers.wsgi import WSGIRequest
-from django.core.serializers import serialize
-from rest_framework.decorators import authentication_classes
 from rest_framework.response import Response
 
 from .base import BaseList, BaseDetail
