2dfbcea7089c5aec924b9adaf655ba1647e68ba9
Kuchuk Egor <kriptyashka@gmail.com>
2024-11-15 15:04:58 +0300

    Расширенная информация о работнике

--- app/api/api_views.py
+++ app/api/api_views.py
@@ -1,4 +1,7 @@
+from django.core.handlers.wsgi import WSGIRequest
+from django.core.serializers import serialize
 from rest_framework.decorators import authentication_classes
+from rest_framework.response import Response
 
 from .base import BaseList, BaseDetail
 from app.models import (
@@ -40,3 +43,31 @@ class EmployeeList(BaseList):
 class EmployeeDetail(BaseDetail):
     def __init__(self, **kwargs):
         super().__init__(EmployeeModel, EmployeeSerializer, **kwargs)
+
+    @staticmethod
+    def extend(employee: EmployeeModel, data: dict) -> dict:
+        extra_departament = {
+            "name": employee.departament.name,
+        }
+        data["departament"] = extra_departament
+
+        extra_position = [{
+            "name": position.name,
+            "salary": position.salary,
+        } for position in employee.position.all()]
+        data["position"] = extra_position
+
+        extra_permission = [{
+            "name": permission.name,
+            "description": permission.description,
+            "code": permission.code,
+        } for permission in employee.permissions.all()]
+        data["permissions"] = extra_permission
+
+        return data
+
+    def get(self, request: WSGIRequest, pk: int):
+        employee = self.get_object(pk)
+        serializer = self.serializer(employee)
+        data = self.extend(employee, serializer.data)
+        return Response(data)
